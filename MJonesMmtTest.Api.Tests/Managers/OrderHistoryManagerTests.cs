ad43d9fb3bf0572beb22f30c73303ce036ea00fa
aceltuk <mark@idevelop.uk.net>
2021-06-13 12:56:56 +0100

    I would never normally push this non-compiling result to a repository, but I have done so to show the initial start of my process - my first test forces me to design the outer parts/edges of my solution so that I can start to Mock/Stub and then create my dependencies. Now to get this solution to build and the first test to pass...

---
+++ MJonesMmtTest.Api.Tests/Managers/OrderHistoryManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace MJonesMmtTest.Api.Tests.Managers
+{
+    public class OrderHistoryManagerTests
+    {
+        [Fact]
+        [Trait("TestType", "Unit")]
+        public void GetLastOrder_InvalidRequest_ReturnsRequestValidationException()
+        {
+            //arrange
+            var customerService = new Mock<ICustomerService>();
+            var orderService = new Mock<IOrderService>();
+            var orderHistoryManager = new OrderHistoryManager(customerService.Object, orderService.Object);
+
+            //act
+            Func<Task> f = async () => { await orderHistoryManager.GetLastOrder(string.Empty, string.Empty); };
+
+            //assert
+            f.Should().Throw<RequestValidationException>();
+        }
+    }
+}
